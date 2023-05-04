74c721c4ceb5e9fc42327bc8f8aa06e4a7f5fbe5
zigBalthazar <zigbalthazar@gmail.com>
2023-05-04 11:28:19 +0330

    init fixture for newNoteBook

--- cypress/e2e/tests/newNoteBook.cy.ts
+++ cypress/e2e/tests/newNoteBook.cy.ts
@@ -6,17 +6,23 @@ before(() => {
   cy.clearCookie("deviceId");
   cy.setCookie("deviceId", "Automation_Test");
   cy.visit(LOCAL_NEW_NOTEBOOK);
+  cy.wait(15000)
 });
 
 beforeEach(() => {
   cy.clearCookie("deviceId");
   cy.setCookie("deviceId", "Automation_Test");
-  cy.viewport(375, 665);
 });
 
 describe("FAIMS3 => New NoteBook", () => {
 
-  it("Should have correct url", () => {
-    cy.url({ timeout: 35000 }).should("eq", LOCAL_NEW_NOTEBOOK);
+    it("Should have correct url", () => {
+      cy.url({ timeout: 35000 }).should("eq", LOCAL_NEW_NOTEBOOK);
+    });
+
+    it("fill Info/General/Project Name field", () => {
+      cy.fixture("newNoteBook.json").then(function (data) {
+      cy.get("#name").type(data.projectName);
+    });
   });
 });
