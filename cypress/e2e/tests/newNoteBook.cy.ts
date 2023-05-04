f721148eece0866c98e150263c158f0a686ba669
zigBalthazar <zigbalthazar@gmail.com>
2023-05-04 11:55:36 +0330

    change loadin fixture

--- cypress/e2e/tests/newNoteBook.cy.ts
+++ cypress/e2e/tests/newNoteBook.cy.ts
@@ -2,11 +2,17 @@
 
 import { LOCAL_NEW_NOTEBOOK } from "../constants/routes/URL";
 
+let noteBook;
+
 before(() => {
   cy.clearCookie("deviceId");
   cy.setCookie("deviceId", "Automation_Test");
   cy.visit(LOCAL_NEW_NOTEBOOK);
-  cy.wait(15000)
+  cy.wait(15000);
+  //* load Fixture
+  cy.fixture("newNoteBook.json").then(function (data) {
+    noteBook = data;
+  });
 });
 
 beforeEach(() => {
@@ -15,14 +21,20 @@ beforeEach(() => {
 });
 
 describe("FAIMS3 => New NoteBook", () => {
+  it("Should have correct url", () => {
+    cy.url({ timeout: 35000 }).should("eq", LOCAL_NEW_NOTEBOOK);
+  });
 
-    it("Should have correct url", () => {
-      cy.url({ timeout: 35000 }).should("eq", LOCAL_NEW_NOTEBOOK);
-    });
-
-    it("fill Info/General/Project Name field", () => {
-      cy.fixture("newNoteBook.json").then(function (data) {
-      cy.get("#name").type(data.projectName);
-    });
+  it("fill Info/General/Project Name field", () => {
+    cy.get("#name").type(noteBook.projectName);
+  });
+  it("fill Info/General/Description field", () => {
+    cy.get("#pre_description").type(noteBook.description);
+  });
+  it("fill Info/General/lead Name field", () => {
+    cy.get("#project_lead").type(noteBook.lead);
+  });
+  it("fill Info/General/leadInstitution field", () => {
+    cy.get("#lead_institution").type(noteBook.leadInstitution);
   });
 });
