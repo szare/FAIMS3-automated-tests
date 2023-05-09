6b7c80b731e6292078d0ecd7b1c85bd48cbd51ee
zigBalthazar <zigbalthazar@gmail.com>
2023-05-09 12:39:02 +0330

    migrate cypres tests from local to git

--- cypress/e2e/tests/newNoteBook.cy.ts
+++ cypress/e2e/tests/newNoteBook.cy.ts
@@ -37,4 +37,55 @@ describe("FAIMS3 => New NoteBook", () => {
   it("fill Info/General/leadInstitution field", () => {
     cy.get("#lead_institution").type(noteBook.leadInstitution);
   });
+  it("click on GO TO NEXT(GENERAL form)", () => {
+    cy.get("#gotonext_info").click({ force: true });
+  });
+  it("Should have same url", () => {
+    cy.url({ timeout: 35000 }).should("eq", LOCAL_NEW_NOTEBOOK);
+  });
+  it("add 3 new meta component", () => {
+    cy.get("#metaadd").type(noteBook.meta1)
+    cy.get("#AddMetaButton").click({ force: true });
+    cy.get("#metaadd").clear().type(noteBook.meta2)
+    cy.get("#AddMetaButton").click({ force: true });
+    cy.get("#metaadd").clear().type(noteBook.meta3)
+    cy.get("#AddMetaButton").click({ force: true });
+  });
+
+  it("add duplicate new meta component", () => {
+    cy.get("#metaadd").type(noteBook.meta1).clear()
+    cy.get("#AddMetaButton").click({ force: true });
+  });
+
+  it("count metas count", () => {
+    cy.get("input").should("have.length", 4);
+  });
+  it("click on GO TO NEXT(META form)", () => {
+    cy.get("#gotonext_info").click({ force: true });
+  });
+  it("Should have same url", () => {
+    cy.url({ timeout: 35000 }).should("eq", LOCAL_NEW_NOTEBOOK);
+  });
+  it("count roles count", () => {
+    cy.get('[class="MuiListItem-root MuiListItem-gutters MuiListItem-padding css-bhlhm1-MuiListItem-root"]').should("have.length", 3);
+  });
+
+  it("add 3 new role component", () => {
+    cy.get("#accessadded").type(noteBook.role1)
+    cy.get("#AddUserRoleButton").click({ force: true });
+    cy.get("#accessadded").clear().type(noteBook.role2)
+    cy.get("#AddUserRoleButton").click({ force: true });
+    cy.get("#accessadded").clear().type(noteBook.role3)
+    cy.get("#AddUserRoleButton").click({ force: true })
+  });
+  it("count added roles count", () => {
+    cy.get('[class="MuiListItem-root MuiListItem-gutters MuiListItem-padding MuiListItem-secondaryAction css-1n8t684-MuiListItem-root"]').should("have.length", 3);
+  });
+
+  it("click on GO TO NEXT(USER ROLE form)", () => {
+    cy.get("#gotonext_info").click({ force: true });
+  });
+  it("Should have same url", () => {
+    cy.url({ timeout: 35000 }).should("eq", LOCAL_NEW_NOTEBOOK);
+  });
 });
