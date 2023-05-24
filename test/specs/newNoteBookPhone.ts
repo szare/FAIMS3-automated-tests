71e65bdfde1114bc607ddab8fe003146bf652105
zigBalthazar <zigbalthazar@gmail.com>
2023-05-24 08:54:07 +0330

    refactor

--- test/specs/newNoteBookPhone.ts
+++ test/specs/newNoteBookPhone.ts
@@ -29,18 +29,18 @@ describe('just for test inspector', () => {
 
     it('New noteBook - INFO', async () => {
             //? GENERAL
-            // await driver.touchAction([ //Scroll down(for xPath)
-            //     { action: 'longPress', x: 0, y: 1500 }, 
-            //     { action: 'moveTo', x: 0, y: 5 },
-            //     'release'
-            // ])
-            //fill Inputs
+            await driver.touchAction([ //Scroll down(for xPath)
+                { action: 'longPress', x: 0, y: 1500 }, 
+                { action: 'moveTo', x: 0, y: 5 },
+                'release'
+            ])
+            //* fill Inputs
             const projectName = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.projectName)
             const desc = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.desc)
             const lead = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[3]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.lead)
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
             const leadInst = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[4]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.leadInstitiution)
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
             //! GO TO NEXT
             const goToNextGeneral = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.Button')).click()
             //? META
@@ -53,47 +53,29 @@ describe('just for test inspector', () => {
             const goToNextInfo = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.Button')).click()
 
             //? USER ROLE
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
             const newRole = await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.widget.EditText')
             const addRole = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.widget.Button'))
             await newRole.setValue(newNoteBookFixture.newRole)
             await addRole.click()
             await newRole.clearValue()
             //! GO TO NEXT
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
             const goToNextUserRole = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.Button')).click()
             //? ATTACHMENT
             const attach = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View[2]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View')).click()
             await driver.pause(2000); // Pause for 3 second
             const selectImage = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.drawerlayout.widget.DrawerLayout/android.widget.ScrollView/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout[1]/android.widget.TextView')).click()
-            await driver.pause(5000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(5000); // Pause for 5 second to load image
             
-            // await driver.touchAction([ //Scroll down(for xPath)
-            //     { action: 'longPress', x: 0, y: 1500 },
-            //     { action: 'moveTo', x: 0, y: 5 },
-            //     'release'
-            // ])
+            await driver.touchAction([ //Scroll down(for xPath)
+                { action: 'longPress', x: 0, y: 1500 },
+                { action: 'moveTo', x: 0, y: 5 },
+                'release'
+            ])
             //! GO TO NEXT
             const goToNextAttachment = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[1]/android.view.View/android.view.View/android.view.View/android.view.View/android.view.View/android.widget.Button')).click()
             
-            //? DESIGN
-
-
-        //     await driver.pause(5000); // Pause for 3 second
-        //     await driver.touchAction([ //Scroll down(for xPath)
-        //     { action: 'longPress', x: 0, y: 350 },
-        //     { action: 'moveTo', x: 0, y: 2200 },
-        //     'release'
-        // ])
-                //! set context
-                const contexts = await driver.getContexts(); // get list of context
-                await driver.switchContext(contexts[0].toString()); // set context to APP_NATIVE
-        await driver.pause(2000); // Pause for 3 second
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-        console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-        await driver.pause(2000); // Pause for 3 second
-        const test = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[4]')).click()
-        // const test = (await $('/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View/android.view.View[3]/android.view.View')).click()
 
 
             await driver.pause(25000); // Pause for 25 second just for trace
--- test/specs/newNoteBookTablet.ts
+++ test/specs/newNoteBookTablet.ts
@@ -23,66 +23,51 @@ describe('just for test inspector', () => {
         await driver.pause(3000); // Pause for 3 second    
         const cr = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.widget.Button')).click()
         await driver.pause(3000); // Pause for 3 second 
-    
         })
 
     it('New noteBook - INFO', async () => {
             //? GENERAL
-            // await driver.touchAction([ //Scroll down(for xPath)
-            //     { action: 'longPress', x: 0, y: 1500 }, 
-            //     { action: 'moveTo', x: 0, y: 5 },
-            //     'release'
-            // ])
-            //fill Inputs
+            //* fill Inputs
             const projectName = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.projectName)
             const desc = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.desc)
             const lead = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[3]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.lead)
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
             const leadInst = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[4]/android.view.View/android.view.View[2]/android.widget.EditText')).setValue(newNoteBookFixture.leadInstitiution)
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
+
             //! GO TO NEXT
             const goToNextGeneral = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.Button')).click()
+           
             //? META
             const newMetaDataLabel = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.widget.EditText'))
             const addMetaData = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.widget.Button'))
             await newMetaDataLabel.setValue(newNoteBookFixture.newMeta)   
             await addMetaData.click()
             await newMetaDataLabel.clearValue()
+
             // ! GO TO NEXT
             const goToNextInfo = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.Button')).click()
 
             //? USER ROLE
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
             const newRole = await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[1]/android.view.View/android.view.View[2]/android.widget.EditText')
             const addRole = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View[2]/android.widget.Button'))
             await newRole.setValue(newNoteBookFixture.newRole)
             await addRole.click()
             await newRole.clearValue()
+
             //! GO TO NEXT
-            await driver.pause(1000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(1000); // Pause for 1 second to load elements
+
             const goToNextUserRole = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.Button')).click()
             //? ATTACHMENT
             const attach = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.view.View')).click()
-            await driver.pause(2000); // Pause for 3 second
+            await driver.pause(2000); // Pause for 2 second
             const selectImage = (await $('//android.view.ViewGroup[@content-desc="Caption bar of Files."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.view.ViewGroup/androidx.drawerlayout.widget.DrawerLayout/android.view.ViewGroup/android.widget.FrameLayout/android.widget.FrameLayout[2]/android.widget.LinearLayout/android.view.ViewGroup/androidx.recyclerview.widget.RecyclerView/android.widget.LinearLayout[1]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.TextView')).click()
-            await driver.pause(5000); // Pause for 3 second to load FAIMS3-Auth
+            await driver.pause(5000); // Pause for 3 second to load image
             
-            // await driver.touchAction([ //Scroll down(for xPath)
-            //     { action: 'longPress', x: 0, y: 1500 },
-            //     { action: 'moveTo', x: 0, y: 5 },
-            //     'release'
-            // ])
             //! GO TO NEXT
             const goToNextAttachment = (await $('//android.view.ViewGroup[@content-desc="Caption bar of FAIMS3."]/android.widget.LinearLayout[1]/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.view.ViewGroup/android.webkit.WebView/android.webkit.WebView/android.view.View/android.view.View/android.view.View[2]/android.view.View[3]/android.view.View/android.view.View/android.view.View[2]/android.view.View/android.widget.Button')).click()
-            
-            //? DESIGN
 
-
-        //     await driver.pause(5000); // Pause for 3 second
-        //     await driver.touchAction([ //Scroll down(for xPath)
-        //     { action: 'longPress', x: 0, y: 350 },
-        //     { action: 'moveTo', x: 0, y: 2200 },
-        //     'release'
-        // ])
     })
 })
